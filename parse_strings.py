6ea60dc6f20366b2a2a3ab6c8677c85bb96ab08f
Gulmira <miramaxeast@yahoo.com>
2015-11-30 20:43:47 -0800

    Adding part 2 parse exercises

--- parse_strings.py
+++ parse_strings.py
@@ -1,17 +1,35 @@
-my_name = "Abla"
-list(my_name)
+# my_name = "Abla"
+# list(my_name)
 
-string = "1,2,3,4,5"
-my_list = []
-for el in string.split(','):
-	my_list.append(int(el))
+# string = "1,2,3,4,5"
+# my_list = []
+# for el in string.split(','):
+# 	my_list.append(int(el))
 
-print my_list
+# print my_list
 
-string = "One fish two fish red fish blue fish"
-my_list = []
-for item in string.split('fish'):
-    my_list.append(item)
+# string = "One fish two fish red fish blue fish"
+# my_list = []
+# for item in string.split('fish'):
+#     my_list.append(item)
 
-print my_list
+# print my_list
 
+str = "item:apples,quantity:4,price:1.50\n"
+split_str = str.split(",")
+print split_str
+
+quantity_list = split_str[1].split(":")
+print quantity_list
+print quantity_list[1]
+quantity = int(quantity_list[1])
+
+amount_list = split_str[2].split(":")
+print amount_list
+price = amount_list[1].strip() 
+print price
+
+price_float = float(price)
+
+print price_float
+print price_float*quantity
